92bddcff7468e3617e9a0c3c0e57f13f10546d4a
Abijith26 <abimurugan.c@gmail.com>
2024-07-27 12:58:37 +0530

    Adding the common styles in a separate index.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,41 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: lime;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
+    <StyledApp>
+      <H1>The Resto Place</H1>
+      <Button>Check In</Button>
+      <Input type="text" placeholder="Enter no.of Guests"></Input>
       <p>Hi</p>
-    </div>
+    </StyledApp>
   );
 }
 
